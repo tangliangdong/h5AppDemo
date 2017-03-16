46a27deed9f8b5d9d9f47a4554aa58e62dfdfbb9
唐良栋 <18868748898@163.com>
2017-03-16 09:34:13 +0800

    h5App

--- js/index.js
+++ js/index.js
@@ -6,17 +6,14 @@ $(function() {
     $('#fullpage').fullpage({
         'verticalCentered': false,
         'css3': true,
-        anchors: ['page1', 'page2', 'page3', 'page4','page5','page6'],
-        'navigation': true,
-        'navigationPosition': 'right',
-        'navigationColor' : '#fff',
-        'navigationTooltips': ['fullPage.js', 'Powerful', 'Amazing', 'Simple'],
+        anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
+        'navigationTooltips': ['BiuBiu组队', '活动页面', '好友页面', '发布页面', '活动详情页面'],
+        scrollingSpee: 1000,
         afterLoad: function(anchorLink, index) {
             switch (index) {
                 case 1:
-                    setTimeout(function() {
-                        secondPageRemoveClass();
-                    }, 500);
+                    secondPageRemoveClass();
+
                     init();
                     break;
                 case 2:
@@ -25,36 +22,39 @@ $(function() {
                         thirdPageRemoveClass();
                     }, 500);
 
-                    $('#page2-box').children('.img1').addClass('animated slideInDown');
+                    $('.page2>.left>.label').addClass('wordTensile');
+                    $('.page2>.left>.thinkSomething').css('transform', 'scale(1)');
                     setTimeout(function() {
-                        $('#page2-box').children('.img2').addClass('animated slideInDown');
-                    }, 100);
-                    setTimeout(function() {
-                        $('#finger').addClass('finger-animate');
+                        $('.page2>.left>.thinkSomething>img').css('opacity', 1);
+
+                        setTimeout(function() {
+                            $('.page2>.left>.travel>h2').css({
+                                'transform': 'translateX(0)',
+                                'opacity': 1
+                            });
+
+                            $('.page2>.right>.img1').addClass('imgTransform');
+                            setTimeout(function() {
+                                $('.page2>.right>.img2').addClass('imgTransform');
+                                setTimeout(function() {
+                                    $('.page2>.right>.img3').addClass('imgTransform');
+                                }, 1000);
+                            }, 1000);
+
+                            setTimeout(function() {
+                                $('.page2>.left>.travel>span').css('width', '90%');
+                            }, 500);
+                        }, 500);
                     }, 1000);
 
-                    $('#page2-describe').children('p').eq(0).addClass('animated rotateInUpRight');
-                    setTimeout(function() {
-                        $('#page2-describe').children('p').eq(1).addClass('animated rotateInUpRight');
-                    }, 100);
+
                     break;
                 case 3:
+                    secondPageRemoveClass();
                     setTimeout(function() {
-                        secondPageRemoveClass();
                         forthPageRemoveClass();
                     }, 500);
-                    $('#page3-box').children('.img1').addClass('animated bounceInDown');
-                    setTimeout(function() {
-                        $('#page3-box').children('.img2').addClass('animated bounceInDown');
-                    }, 100);
-                    setTimeout(function() {
-                        $('#page3-finger').addClass('finger-animate');
-                    }, 1000);
 
-                    $('#page3-describe').children('p').eq(0).addClass('animated flipInX');
-                    setTimeout(function() {
-                        $('#page3-describe').children('p').eq(1).addClass('animated flipInX');
-                    }, 100);
 
                     break;
                 case 4:
@@ -62,81 +62,86 @@ $(function() {
                         fifthPageRemoveClass();
                         thirdPageRemoveClass();
                     }, 500);
-                    $('#page4-box').children('.card').addClass('obliqueTopAndRight');
 
-                    setTimeout(function(){
-                      $('#page4-box').children('.card').children('.img3').addClass('toTheRight');
-                      setTimeout(function(){
-                        $('#page4-box').children('.card').children('.img2').addClass('toTheLeft');
-                        setTimeout(function(){
-                          $('#page4-describe').children('p').addClass('obliqueBottomAndLeft');
-                          setTimeout(function(){
-                            setTimeout(function(){
-                              $('#page4-describe').children('.finger-upward').addClass('fingerB');
-                            },1400);
-                          },1000);
-                        },600);
-                      },1200);
-                    },1000);
 
 
                     break;
-                    case 5:
+                case 5:
                     setTimeout(function() {
                         forthPageRemoveClass();
                         sixthPageRemoveClass();
                     }, 500);
-                      break;
+
+
+                    break;
             }
         }
     });
     init();
+
+    var audio = $('#bgMusic');
+    $('#music-play').css('display', 'block');
+    var tab = true;
+    $('#music-play').click(function(event) {
+      if(tab){
+        audio.get(0).play();
+        tab = false;
+      }else{
+        audio.get(0).pause();
+        tab = true;
+      }
+    });
+
 });
 
-function onePageRemoveClass() {
-    var $welcomePage = $('#welcome-page');
-    $welcomePage.children('img').css('opacity', 0);
-    $welcomePage.children('h2').removeClass('animated slideInDown');
-    $welcomePage.children('p').removeClass('animated slideInDown');
-    $welcomePage.children('img').removeClass('animated bounceInLeft');
-}
-
 function init() {
-    var $welcomePage = $('#welcome-page');
-
-    $welcomePage.children('h2').addClass('animated slideInDown');
-    $welcomePage.children('p').addClass('animated slideInDown');
-    $welcomePage.children('img').addClass('animated bounceInLeft');
-    $('#welcome-page').children('img').css('opacity', 1);
+    $('.page1>.title').addClass('animated fadeInDown');
+    setTimeout(function() {
+        $('.page1>.vertical').addClass('animated verticalSpread');
+        $('.page1>.horizontal').addClass('animated horizontalSpread');
+        setTimeout(function() {
+            $('.page1>.img1').addClass('animated imgRotate');
+            $('.page1>.img2').addClass('animated imgRotate');
+            setTimeout(function() {
+                $('.page1>.signature').addClass('animated fadeInDown');
+            }, 1000);
+        }, 500);
+    }, 500);
 }
+
+function onePageRemoveClass() {
+    $('.page1>.title').removeClass('animated fadeInDown');
+    $('.page1>.vertical').removeClass('animated verticalSpread');
+    $('.page1>.horizontal').removeClass('animated horizontalSpread');
+    $('.page1>.img1').removeClass('animated imgRotate');
+    $('.page1>.img2').removeClass('animated imgRotate');
+    $('.page1>.signature').removeClass('animated fadeInDown');
+}
+
 
 function secondPageRemoveClass() {
-    $('#finger').removeClass('finger-animate');
-    $('#page2-box').children('.img2').removeClass('animated slideInDown');
-    $('#page2-box').children('.img1').removeClass('animated slideInDown');
-    $('#finger').removeClass('finger-animate');
-    $('#page2-describe').children('p').removeClass('animated rotateInUpRight');
+    $('.page2>.left>.label').removeClass('wordTensile');
+    $('.page2>.left>.thinkSomething').css('transform', 'scale(0)');
+    $('.page2>.left>.thinkSomething>img').css('opacity', 0);
+
+    $('.page2>.left>.travel>h2').css({
+        'transform': 'translateX(-100%)',
+        'opacity': 0
+    });
+    $('.page2>.right>img').removeClass('imgTransform');
+    $('.page2>.left>.travel>span').css('width', '0px');
 }
 
+
 function thirdPageRemoveClass() {
-    $('#page3-finger').removeClass('finger-animate');
-    $('#page3-box').children('.img2').removeClass('animated bounceInDown');
-    $('#page3-box').children('.img1').removeClass('animated bounceInDown');
-    $('#finger').removeClass('finger-animate');
-    $('#page3-describe').children('p').removeClass('animated flipInX');
+
 }
 
 function forthPageRemoveClass() {
-  $('#page4-box').children('.card').removeClass('obliqueTopAndRight');
-  $('#page4-describe').children('p').removeClass('obliqueBottomAndLeft');
-  $('#page4-describe').children('.finger-upward').removeClass('fingerB');
-  $('#page4-box').children('.card').children('.img2').removeClass('toTheLeft');
-  $('#page4-box').children('.card').children('.img3').removeClass('toTheRight');
+
 }
 
 function fifthPageRemoveClass() {
 
-}
-function sixthPageRemoveClass(){
 
 }
